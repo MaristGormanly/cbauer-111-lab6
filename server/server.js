e022c90ef5d0626b3162af99d1213c5d7887d567
christianbauer4 <mrchristianbauerusa@gmail.com>
2023-04-04 12:45:42 -0400

    merging lab 5 files with lab 6

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('../client/public'));
+
+app.get('/', function (req, res) {
+	res.sendFile('feed.html', {root: '../client/views' })
+})
+
+app.get('/signup', function (req, res) {
+	res.sendFile('signup.html', {root: '../client/views'})
+})
+
+app.get('/login', function (req, res) {
+	res.sendFile('login.html', {root: '../client/views'})
+})
+
+// Include the userRoutes module
+let userRoutes = require('../server/route/userRoutes')
+
+app.listen(1337, () => console.log('gato listening on port 1337'));
