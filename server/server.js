6e7cfd81049166c7c1ead220fb9fc68d42e0a68d
christianbauer4 <mrchristianbauerusa@gmail.com>
2023-04-04 13:01:09 -0400

    fixing routes

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../client/public'));
+app.use(express.static('./client/public'));
 
 app.get('/', function (req, res) {
-	res.sendFile('feed.html', {root: '../client/views' })
+	res.sendFile('feed.html', {root: './client/views' })
 })
 
 app.get('/signup', function (req, res) {
-	res.sendFile('signup.html', {root: '../client/views'})
+	res.sendFile('signup.html', {root: './client/views'})
 })
 
 app.get('/login', function (req, res) {
-	res.sendFile('login.html', {root: '../client/views'})
+	res.sendFile('login.html', {root: './client/views'})
 })
 
 // Include the userRoutes module
