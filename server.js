6f017e93bda34f657ea2adc34ada7670ac61055a
christianbauer4 <mrchristianbauerusa@gmail.com>
2023-04-04 12:50:12 -0400

    commit msg

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Include the userRoutes module
-let userRoutes = require('./server/route/userRoutes')
-
-app.listen(1337, () => console.log('gato listening on port 1337'));
