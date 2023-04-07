2d6987c984113b24d21cc267ba063777d4b57a9a
christianb4 <mrchristianbauerusa@gmail.com>
2023-04-07 17:41:50 -0400

    adding 2.1 and 2.2 changes

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,3 +1,42 @@
 let user = require('../model/user')
 
 console.log("[userController] initialized")
+
+// create an array to hold the created users
+let users = [];
+
+// creating users
+let christian = User.createUser("Christian", "Bauer");
+let hank = User.createUser("Hank", "Hill");
+let titan = User.createUser("Titan", "Warrior");
+let daquavious = User.createUser("Daquavious", "Bingleton");
+
+// adding the users to the array
+users.push(christian);
+users.push(hank);
+users.push(titan);
+users.push(daquavious);
+
+/* retrieve the user in the :index parameter of the request and return as json,
+lets us return 1 user from the array based on the parameter passed in the url*/
+exports.getUser = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users[req.params.index]);
+}
+
+// save a user by using POST method of the /api/user resource url
+exports.saveUser = (req, res) => {
+    let newUser = User.createUser(req.body.firstName, req.body.lastName);
+    users.push(newUser);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users);
+}
+
+/* send entire users array as the body of the response as json 
+(function will be called when the API call to query and reutrn all users is called) 
+function should be at bottom of controller file */
+exports.getAllUsers = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(users);
+}
+
--- server/model/user.js
+++ server/model/user.js
@@ -1,1 +1,11 @@
-console.log("[user] initialized")
+class User {
+    constructor (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+// adding to exports array to make values accessible where user.js file is used
+exports.createUser = function(firstName, lastName) {
+    return new User(firstName, lastName)
+}
